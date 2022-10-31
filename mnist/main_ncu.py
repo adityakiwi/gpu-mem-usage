56fbe1128611c081d7f2a8b34ad449b21f872dec
Aditya Dixit <ad5157@b-19-10.c.hpc-slurm-9c75.internal>
2022-10-31 15:32:00 -0400

    made updates?

--- mnist/main_ncu.py
+++ mnist/main_ncu.py
@@ -25,7 +25,11 @@ class Net(nn.Module):
     def forward(self, x):
         x = self.conv1(x)
         x = F.relu(x)
+        if epoch == 2:
+            NCU.start()    
         x = self.conv2(x)
+        if epoch == 2:
+            NCU.stop()
         x = F.relu(x)
         x = F.max_pool2d(x, 2)
         x = self.dropout1(x)
@@ -141,7 +145,8 @@ def main():
 
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
     # profiler=torch.profiler.profile(schedule=torch.profiler.schedule(wait=1, warmup=1, active=3, repeat=2), 
-            on_trace_ready=torch.profiler.tensorboard_trace_handler('./log/mnist'), record_shapes=True, profile_memory=True, with_stack=True)
+    on_trace_ready=torch.profiler.tensorboard_trace_handler('./log/mnist')
+    #, record_shapes=True, profile_memory=True, with_stack=True)
     # profiler.start()
     for epoch in range(1, args.epochs + 1):
         if epoch == 2: NCU.start()
