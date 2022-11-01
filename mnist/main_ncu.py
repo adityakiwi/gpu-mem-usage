c72a8a1e8089b5f4e4e71fae22c78faa714a2f99
Aditya Dixit <ad5157@b-19-4.c.hpc-slurm-9c75.internal>
2022-10-31 20:43:35 -0400

    working

--- mnist/main_ncu.py
+++ mnist/main_ncu.py
@@ -22,7 +22,7 @@ class Net(nn.Module):
         self.fc1 = nn.Linear(9216, 128)
         self.fc2 = nn.Linear(128, 10)
 
-    def forward(self, x,epoch):
+    def forward(self, x,epoch=None):
         x = self.conv1(x)
         x = F.relu(x)
         if epoch == 2:
