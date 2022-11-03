e7cfe0e54272226ab1fa63985df7cfae39771b1a
Aditya Dixt <87985672+adityakiwi@users.noreply.github.com>
2022-11-02 23:46:31 -0400

    Update main_ncu.py

--- mnist/main_ncu.py
+++ mnist/main_ncu.py
@@ -87,7 +87,7 @@ def main():
                         help="input batch size for training (default: 64)")
     parser.add_argument("--test-batch-size", type=int, default=1000, metavar="N",
                         help="input batch size for testing (default: 1000)")
-    parser.add_argument("--the_number_of_epochss", type=int, default=14, metavar="N",
+    parser.add_argument("--epochs", type=int, default=14, metavar="N",
                         help="number of the_number_of_epochss to train (default: 14)")
     parser.add_argument("--lr", type=float, default=1.0, metavar="LR",
                         help="learning rate (default: 1.0)")
